9aa4b39b94679e3e319df2b896a8f8cc8d2c90f1
Kari idskov holm Madsen <kama7@elev.techcollege.dk>
2025-08-25 13:29:10 +0200

    opgave2

--- assets/js/opgaver/arrays.js
+++ assets/js/opgaver/arrays.js
@@ -9,5 +9,8 @@ const myArray = ["element1", "element2","element3","element4","element5" ]
 
 // opgave 2 console.logs
 
+const myArray = ["element3", "element5","element1","element4","element2" ]
+console.log
+
 
 
